88cbb83408aff0c71036c105429507ae6b708728
Kushika <118838567+Kushika04@users.noreply.github.com>
2023-10-07 23:53:32 +0530

    Lecture-3

---
+++ lect3.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // this is a header
+/*
+comment
+lines
+*/
+using namespace std;
+int main(){                  //we have make a main "function"
+    cout<<"Hello World";
+    return 0;
+}
