a2c30c0c4feb60012ab7fc0634d8baa06ad4da01
Kushika <118838567+Kushika04@users.noreply.github.com>
2023-10-08 00:27:25 +0530

    Lecture-4

---
+++ lec4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    int c,d;
+    cout<<"enter 4 numbers: ";
+    cin>>a>>b;
+    cin>>c>>d;
+    cout<<(a>b || c>d);
+    return 0;
+}
