250e13561c507ae77cf601a369b11c4be2132031
Kushika <118838567+Kushika04@users.noreply.github.com>
2023-10-08 16:48:49 +0530

    Lecture 5 part1

---
+++ lect-5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e;
+    
+    cin>>a>>b>>c>>d>>e;
+    cout<<"5 integers in reverse order are: "<<e<<" "<<d<<" "<<c<<" "<<b<<" "<<a;
+
+    return 0;
+}
